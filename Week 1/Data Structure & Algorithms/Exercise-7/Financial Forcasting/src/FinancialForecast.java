ed59903b8ee07596258903c4d1229506fff3a59f
Glitched <adityasnkr1@gmail.com>
2026-06-20 19:50:45 +0530

    Completed Exercise 7

---
+++ Week 1/Data Structure & Algorithms/Exercise-7/Financial Forcasting/src/FinancialForecast.java
@@ -0,0 +1,37 @@
+public class FinancialForecast {
+
+    public static double predictFutureValue(
+            double currentValue,
+            double growthRate,
+            int years) {
+
+        // Base Case
+        if (years == 0) {
+            return currentValue;
+        }
+
+        // Recursive Case
+        return predictFutureValue(
+                currentValue * (1 + growthRate),
+                growthRate,
+                years - 1);
+    }
+
+    public static void main(String[] args) {
+
+        double currentValue = 10000;
+        double growthRate = 0.10; // 10%
+        int years = 5;
+
+        double futureValue =
+                predictFutureValue(
+                        currentValue,
+                        growthRate,
+                        years);
+
+        System.out.printf(
+                "Predicted value after %d years = Rs.%.2f",
+                years,
+                futureValue);
+    }
+}
