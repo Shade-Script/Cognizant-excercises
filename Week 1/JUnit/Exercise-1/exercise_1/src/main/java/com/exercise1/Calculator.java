a88524fa57963a1b85ef6a62f29836cc32717c4b
Glitched <adityasnkr1@gmail.com>
2026-06-21 20:43:56 +0530

    Step 3: Upgrade Compiler to Java 25 - Compile: SUCCESS
    Updated pom.xml: maven.compiler.source and maven.compiler.target from 21 to 25.
    No source code changes required - all code is compatible with Java 25.

---
+++ Week 1/JUnit/Exercise-1/exercise_1/src/main/java/com/exercise1/Calculator.java
@@ -0,0 +1,7 @@
+package com.exercise1;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ Week 1/JUnit/Exercise-1/exercise_1/src/test/java/CalculatorTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import com.exercise1.Calculator;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(2, 3));
+    }
+}
